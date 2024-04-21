f7fc3633f718c2af758c86ec1557cc4abf2f5c78
Császár Dániel <csaszardaniel81@gmail.com>
2024-04-21 15:04:03 +0200

    update

---
+++ main.py
@@ -0,0 +1,3 @@
+Trombita_Tab = "G3-A3-H3-C4 D4-E4-F#4-G4 E4-F#4-G4-H4 C5-G4-E4 C5-H4-G4-D4-H4 A4-H4-C5-F#4 G4-A4-H4-H4-A4-G4-A4-E4 G4-A4-H4-A4"
+guitar_tab = Trombita_Tab.replace("G3", "A10").replace("A3", "D7").replace("H3", "D9").replace("C4", "D10").replace("D4", "G7").replace("E4", "G9").replace("F#4", "B7").replace("G4", "B8").replace("H4", "E7").replace("C5", "E8").replace("A4", "B11")
+print(guitar_tab)
